8147ca58163ae705e0b69558348cd596c431a694
yuhkiikd <yuhki.ikd@gmail.com>
2020-08-03 18:48:23 +0900

    メモ作成・更新

---
+++ src/views/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="home">
+    <header>My Memos</header>
+    <ul v-for="memo in newest" :key="memo.id">
+      <li>
+        <router-link :to="{name: 'Edit', params: { id: memo.id }}">
+          {{ memo.body }}
+        </router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style>
+  li {
+    list-style: none;
+  }
+  textarea {
+      width:100%;
+      height: 10em;
+  }
+
+  button {
+      border: 1px solid #333;
+      background-color: #333;
+      color:#fff;
+      padding:10px 20px;
+      margin-top: 10px;
+  }
+</style>
+
+<script>
+  export default{
+    name: "home",
+    computed: {
+      newest: function() {
+        return this.$store.state.memos.slice().reverse();
+      }
+    }
+  }
+</script>
